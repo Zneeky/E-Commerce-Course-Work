48467fd1fc7c91472c16781525e910dc7eed872a
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 19:08:23 +0200

    finished UserRepository

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/UserRepository.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/UserRepository.java
@@ -5,7 +5,13 @@ package com.obuvki.rest.repository;
 import com.obuvki.rest.Models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
+
+
+    Optional<AppUser> findByEmail(String email);
 
 }
