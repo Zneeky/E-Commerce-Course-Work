bb82c829117f8aee1a4a0f0717d690e572e8b10f
Alex Traykov <alextraikov02@gmail.com>
2023-12-19 16:35:47 +0200

    Added AppOrderDetails model

---
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/Models/AppOrderDetails.java
@@ -0,0 +1,20 @@
+package com.obuvki.rest.Models;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class AppOrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private AppOrder orderid;
+
+    @ManyToOne
+    private AppProduct productid;
+
+    @Column
+    private Integer quantity;
+}
