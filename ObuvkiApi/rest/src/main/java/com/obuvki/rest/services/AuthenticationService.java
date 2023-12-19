285be00726ba02be3cbfc0166ebf0f376345361a
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 21:37:07 +0200

    update AuthService and impl

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/AuthenticationService.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/AuthenticationService.java
@@ -1,9 +1,13 @@
 package com.obuvki.rest.services;
 
+import com.obuvki.rest.DTO.JwtAuthenticationResponse;
+import com.obuvki.rest.DTO.SignInRequest;
 import com.obuvki.rest.DTO.SignUpRequest;
 import com.obuvki.rest.Models.AppUser;
 
 public interface AuthenticationService {
 
     AppUser signup(SignUpRequest signUpRequest);
+
+    JwtAuthenticationResponse signin(SignInRequest signInRequest) throws IllegalAccessException;
 }
--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/impl/AuthenticationServiceImpl.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/impl/AuthenticationServiceImpl.java
@@ -1,13 +1,21 @@
 package com.obuvki.rest.services.impl;
 
+import com.obuvki.rest.DTO.JwtAuthenticationResponse;
+import com.obuvki.rest.DTO.SignInRequest;
 import com.obuvki.rest.DTO.SignUpRequest;
 import com.obuvki.rest.Models.AppUser;
 import com.obuvki.rest.Models.Role;
 import com.obuvki.rest.repository.UserRepository;
 import com.obuvki.rest.services.AuthenticationService;
+import com.obuvki.rest.services.JWTService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +25,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final AuthenticationManager authenticationManager;
+
+    private final JWTService jwtService;
+
     public AppUser signup(SignUpRequest signUpRequest){
         AppUser user = new AppUser();
 
@@ -25,5 +37,23 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
 
         return userRepository.save(user);
+    }
+
+
+    public JwtAuthenticationResponse signin(SignInRequest signInRequest) throws IllegalAccessException {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getEmail(),
+                signInRequest.getPassword()));
+
+        var user = userRepository.findByEmail(signInRequest.getEmail())
+                .orElseThrow(() -> new IllegalAccessException("Invalid email or password"));
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(new HashMap<>(),user);
+
+        JwtAuthenticationResponse jwtAuthenticationResponse = new JwtAuthenticationResponse();
+
+        jwtAuthenticationResponse.setToken(jwt);
+        jwtAuthenticationResponse.setRefreshToken(refreshToken);
+
+        return jwtAuthenticationResponse;
     }
 }
