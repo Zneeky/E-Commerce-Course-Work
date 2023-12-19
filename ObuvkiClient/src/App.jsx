b6beb3d98fcee1e86e8ff3c2dcfc6f2dccaaa850
Zneeky <arkanahmedov02@gmail.com>
2023-12-19 17:43:05 +0200

    Products displayed

--- ObuvkiClient/src/App.jsx
+++ ObuvkiClient/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/auth-context'
 import Register from './pages/register-page'
 import Login from './pages/login-page'
+import ProductList from './pages/product-list'
+import { ProductProvider } from './contexts/product-context'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
@@ -9,12 +13,15 @@ function App() {
   return (
     <>
       <AuthProvider>
+        <ProductProvider>
         <div>
           <Routes>
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route exact path="/" element={<ProductList/>} />
           </Routes>
         </div>
+        </ProductProvider>
       </AuthProvider>
     </>
   )
