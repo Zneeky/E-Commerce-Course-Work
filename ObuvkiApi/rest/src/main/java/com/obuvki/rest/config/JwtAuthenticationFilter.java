406f5acd3a8ef8ff25039591d631dad1fa3d4aff
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 19:08:01 +0200

    finished AuthFilter

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/config/JwtAuthenticationFilter.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/config/JwtAuthenticationFilter.java
@@ -1,14 +1,18 @@
 package com.obuvki.rest.config;
 
 import com.obuvki.rest.services.JWTService;
+import com.obuvki.rest.services.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,7 +42,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if(StringUtils.isNoneEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+
+            }
         }
+
+        filterChain.doFilter(request, response);
 
 
     }
--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/UserService.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/UserService.java
@@ -1,4 +1,8 @@
 package com.obuvki.rest.services;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 public interface UserService {
+
+    UserDetailsService userDetailsService();
 }
