0acf1a796398a2eb643be4a09dc7ff8a20762761
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 18:21:13 +0200

    Update User for JWT

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/Models/AppUser.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/Models/AppUser.java
@@ -1,10 +1,17 @@
 package com.obuvki.rest.Models;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 @Entity
 @Data
-public class AppUser {
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="userid")
@@ -24,4 +31,36 @@ public class AppUser {
 
     @OneToOne(mappedBy = "userid")
     private AppCart cartUser;
+
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
