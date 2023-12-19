ce27861cea322d3a3d229599b878b54b77f74d52
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 22:21:30 +0200

    test admin endpoint

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/Controller/AdminController.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/Controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.obuvki.rest.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hi Admin");
+    }
 }
