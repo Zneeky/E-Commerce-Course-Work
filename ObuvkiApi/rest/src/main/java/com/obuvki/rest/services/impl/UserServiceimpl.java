de9ac64f5725323cd6ae88bdd10636463934d337
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2023-12-19 18:48:20 +0200

    add UserService interface and impl

---
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/services/impl/UserServiceimpl.java
@@ -0,0 +1,10 @@
+package com.obuvki.rest.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceimpl {
+    
+}
