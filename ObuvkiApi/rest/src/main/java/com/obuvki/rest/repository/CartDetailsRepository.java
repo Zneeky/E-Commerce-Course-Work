a888d9e386f4b1988ca7a58068ca6109bdba0755
Alex Traykov <alextraikov02@gmail.com>
2023-12-22 19:57:44 +0200

    Added functions to CartDetailsRepository and ProductRepository

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/CartDetailsRepository.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/CartDetailsRepository.java
@@ -1,8 +1,14 @@
 package com.obuvki.rest.repository;
 
+import com.obuvki.rest.Models.AppCart;
 import com.obuvki.rest.Models.AppCartDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface CartDetailsRepository extends JpaRepository<AppCartDetails, Long> {
+    List<AppCartDetails> findByCartId(long cartId);
 
+    AppCartDetails findByProductIdAndCartId(long productId, long cartId);
+    void deleteByProductIdAndCartId(long productId, long cartId);
+
+    void deleteByCartId(long cartId);
 }
--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/ProductRepository.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/repository/ProductRepository.java
@@ -1,12 +1,10 @@
 package com.obuvki.rest.repository;
 
-import com.obuvki.rest.DTO.ProductDTOs.ExportProductDto;
 import com.obuvki.rest.Models.AppProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<AppProduct, Long> {
-
+    List<AppProduct> findAppProductByIdIn(List<Long> ids);
 }
