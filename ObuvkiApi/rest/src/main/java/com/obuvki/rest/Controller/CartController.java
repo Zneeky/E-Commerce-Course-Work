554b0824036ca0326c974415c9b59911278cd960
Alex Traykov <alextraikov02@gmail.com>
2023-12-22 19:53:36 +0200

    Added more endpoints to CartController

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/Controller/CartController.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/Controller/CartController.java
@@ -1,8 +1,11 @@
 package com.obuvki.rest.Controller;
 
+import com.obuvki.rest.DTO.ProductDTOs.ExportCartDto;
 import com.obuvki.rest.services.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -14,4 +17,27 @@ public class CartController {
         cartService.addProductToCart(productId,userId);
     }
 
+    @GetMapping("/{userId}")
+    public List<ExportCartDto> getAllCartProducts(@PathVariable long userId){
+        return cartService.getCartProducts(userId);
+    }
+    @DeleteMapping("/deleteInCart")
+    public void deleteProductInCart(@RequestParam("productId") long productId, @RequestParam("userId") long userId){
+        cartService.deleteProductInCart(productId, userId);
+    }
+
+    @DeleteMapping("/deleteAllInCart")
+    public void deleteAllInCart(@RequestParam("userId") long userId){
+        cartService.deleteAllProducts(userId);
+    }
+
+    @PatchMapping("/addQuantity")
+    public void addQuantityToProduct(@RequestParam("productId") long productId, @RequestParam("userId") long userId){
+        cartService.increaseProductQuantity(productId, userId);
+    }
+
+    @PatchMapping("/decreaseQuantity")
+    public void decreaseQuantityToProduct(@RequestParam("productId") long productId, @RequestParam("userId") long userId){
+        cartService.decreaseProductQuantity(productId, userId);
+    }
 }
