d2886be6709f2dbb7f133b45f089632af968d8f5
Alex Traykov <alextraikov02@gmail.com>
2023-12-22 19:56:33 +0200

    Added ExportCartDto

--- ObuvkiApi/rest/src/main/java/com/obuvki/rest/DTO/ProductDTOs/ExportCartDto.java
+++ ObuvkiApi/rest/src/main/java/com/obuvki/rest/DTO/ProductDTOs/ExportCartDto.java
@@ -2,5 +2,10 @@ package com.obuvki.rest.DTO.ProductDTOs;
 import lombok.Data;
 @Data
 public class ExportCartDto {
-
+    private  long id;
+    private  String productName;
+    private  float price;
+    private  String size;
+    private  long quantity;
+    private  String imageURL;
 }
