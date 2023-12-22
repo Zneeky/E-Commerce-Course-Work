40e9f9c89e613d82e854ad42c1c72f1ac7972e05
Chavdar <cavdar2002@gmail.com>
2023-12-22 13:50:13 +0200

    navbar update

--- ObuvkiClient/src/components/navbar/navbar.jsx
+++ ObuvkiClient/src/components/navbar/navbar.jsx
@@ -2,36 +2,21 @@ import { Component } from 'react'
 import {Link} from 'react-router-dom';
 //import logo from '../logo.svg';
 import styled from 'styled-components';
-import {ButtonContainer} from '../button';
+import './navbar.css'
 
 export default class Navbar extends Component {
     render() {
         return (
-            <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
-               {/* <Link to='/'>
-                  <img src={logo} alt="store" className="navbar-brand" />
-               </Link>  */}
-               <ul className="navbar-nav align-items-center">
-                 <li className="nav-item ml-5">
-                    <Link to="/" className="nav-link">
-                       Products
-                    </Link>
-                 </li>
-               </ul>
+            <div className="nav-container">
+               {<Link to='/'>
+                  <img src={'/src/assets/logo.png'} alt="store"  className="logo" />
+               </Link>}
                <Link to="/cart" className="ml-auto">
-                  <ButtonContainer>
+                  <div className="button-container">
                       <i className="fas fa-cart-plus">my cart</i>
-                  </ButtonContainer>
+                  </div>
                </Link>
-            </NavWrapper>
+            </div>
         )
     }
 }
-const NavWrapper = styled.nav`
-background:var(--mainBlue);
-.nav-link{
-    color:var(--mainWhite) !important;
-    font-size:1.3 rem;
-    text-transform:capitalize;
-}
-`;
